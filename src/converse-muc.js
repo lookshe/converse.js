efc7aebfede448bc9e0e9039a20c036c210d8f71
JC Brand <jc@opkode.com>
2016-12-04 13:05:56 +0100

    Remove unused promises
    Now that we cache disco info and not the room configuration.

--- src/converse-muc.js
+++ src/converse-muc.js
@@ -886,7 +886,6 @@
                      *  (XMLElement) stanza: The IQ stanza containing the room config.
                      */
                     var that = this,
-                        deferred = new $.Deferred(),
                         $body = this.$('.chatroom-body');
                     $body.children().addClass('hidden');
                     $body.append(converse.templates.chatroom_form());
@@ -912,15 +911,11 @@
                     $fieldset.find('input[type=button]').on('click', function (ev) {
                         ev.preventDefault();
                         that.cancelConfiguration();
-                        deferred.reject(stanza);
                     });
                     $form.on('submit', function (ev) {
                         ev.preventDefault();
-                        that.saveConfiguration(ev.target)
-                            .done(deferred.resolve)
-                            .fail(_.partial(deferred, stanza));
+                        that.saveConfiguration(ev.target);
                     });
-                    return deferred.promise();
                 },
 
                 sendConfiguration: function(config, onSuccess, onError) {
@@ -956,25 +951,19 @@
                      * Parameters:
                      *  (HTMLElement) form: The configuration form DOM element.
                      */
-                    var deferred = new $.Deferred();
                     var that = this;
                     var $inputs = $(form).find(':input:not([type=button]):not([type=submit])'),
                         configArray = [];
                     $inputs.each(function () {
                         configArray.push(utils.webForm2xForm(this));
                     });
-                    this.sendConfiguration(
-                        configArray,
-                        deferred.resolve,
-                        deferred.reject
-                    );
+                    this.sendConfiguration(configArray);
                     this.$el.find('div.chatroom-form-container').hide(
                         function () {
                             $(this).remove();
                             that.$el.find('.chat-area').removeClass('hidden');
                             that.$el.find('.occupants').removeClass('hidden');
                         });
-                    return deferred.promise();
                 },
 
                 autoConfigureChatRoom: function (stanza) {
@@ -988,7 +977,6 @@
                      *  (XMLElement) stanza: IQ stanza from the server,
                      *       containing the configuration.
                      */
-                    var deferred = new $.Deferred();
                     var that = this, configArray = [],
                         $fields = $(stanza).find('field'),
                         count = $fields.length,
@@ -1014,14 +1002,9 @@
                         }
                         configArray.push(this);
                         if (!--count) {
-                            that.sendConfiguration(
-                                configArray,
-                                deferred.resolve,
-                                _.partial(deferred.reject, stanza)
-                            );
+                            that.sendConfiguration(configArray);
                         }
                     });
-                    return deferred.promise();
                 },
 
                 cancelConfiguration: function () {
@@ -1675,6 +1658,8 @@
                             'label_occupants': __('Occupants')
                         })
                     );
+                    // TODO: don't allow the widget if members-only room and
+                    // not room owner.
                     if (converse.allow_muc_invitations) {
                         return this.initInviteWidget();
                     }
